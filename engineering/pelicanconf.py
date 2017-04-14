60f733bb37d2567f1e49ced5ffdf4591b09e00b8
Extellion <allan@guichard.biz>
2017-04-14 12:48:10 +0200

    Added static blog generator Pelican

---
+++ engineering/pelicanconf.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+# Basic config
+AUTHOR = u'Florent Messa'
+SITENAME = u'Ulule'
+SITEURL = ''
+PATH = 'content'
+TIMEZONE = 'Europe/Paris'
+
+# Define theme and static path to img folder
+THEME = 'themes/Ulule'
+STATIC_PATHS = ['themes/Ulule/static/img',]
+
+DEFAULT_DATE_FORMAT = '%d %B %Y'
+
+# Remove all unwanted files
+AUTHORS_SAVE_AS = ''
+CATEGORIES_SAVE_AS = ''
+TAGS_SAVE_AS = ''
+ARCHIVES_SAVE_AS = ''
+FEEDS_SAVE_AS = ''
+AUTHOR_SAVE_AS = ''
+THEME_SAVE_AS = ''
+CATEGORY_SAVE_AS = ''
+FEED_DOMAIN = None
+FEED_ATOM = None
+FEED_RSS = None
+FEED_ALL_ATOM = None
+FEED_ALL_RSS = None
+CATEGORY_FEED_ATOM = None
+CATEGORY_FEED_RSS = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+TAG_FEED_ATOM = None
+TAG_FEED_RSS = None
+
+
+DEFAULT_LANG = u'en'
+
+# Blogroll
+LINKS = (('Ulule', 'http://ulule.com/')),
+
+# Social links
+SOCIAL = (('Twitter', 'https://twitter.com/ulule'),
+          ('Github', 'https://github.com/ulule'),
+          ('Facebook', 'https://facebook.com/ulule'),)
